355e03d2f1ca6aa0bcf9ddcad9021a345f95c6ca
amdymstfa <amdymoustapha011@gmail.com>
2025-11-05 15:26:41 +0100

    add new method findAll with pagination

--- src/main/java/com/yallatawsil/backend/service/BaseService/BaseService.java
+++ src/main/java/com/yallatawsil/backend/service/BaseService/BaseService.java
@@ -1,5 +1,8 @@
 package com.yallatawsil.backend.service.BaseService ;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import java.util.List;
 
 public interface BaseService<RequestDTO, ResponseDTO, ID> {
@@ -10,7 +13,10 @@ public interface BaseService<RequestDTO, ResponseDTO, ID> {
 
     List<ResponseDTO> findAll();
 
+    Page<ResponseDTO> findAll(Pageable pageable);
+
     ResponseDTO update(ID id ,RequestDTO dto);
 
     void delete(ID id);
+
 }
