7ce0c66836ee307cd179fed61299b687d4e34579
amdymstfa <amdymoustapha011@gmail.com>
2025-11-05 01:09:28 +0100

    fix(service): mark TourServiceImpl as @Service for Spring injection

--- src/main/java/com/yallatawsil/backend/service/InterfaceEntityImpl/TourServiceImpl.java
+++ src/main/java/com/yallatawsil/backend/service/InterfaceEntityImpl/TourServiceImpl.java
@@ -13,6 +13,7 @@ import com.yallatawsil.backend.service.optimizer.TourOptimizer;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
@@ -21,6 +22,7 @@ import java.util.stream.Collectors;
 @Slf4j
 @Setter
 @Getter
+@Service
 public class TourServiceImpl implements TourService {
 
     private final TourRepository tourRepository;
