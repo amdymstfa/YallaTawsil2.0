74d0ffef1ac941a1a4259c4931f6dd328edb3c62
amdymstfa <amdymoustapha011@gmail.com>
2025-11-05 15:00:28 +0100

    feat(repository): add CustomerRepository with custom queries and statistics

---
+++ src/main/java/com/yallatawsil/backend/repository/CustomerRepository.java
@@ -0,0 +1,63 @@
+package com.yallatawsil.backend.repository ;
+
+import com.yallatawsil.backend.entity.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    /**
+     * Find customer by name
+     * @param  name of customer
+     * @return List of name
+     */
+    public List<Customer> findByNameContainingIgnoreCase(String name);
+
+    /**
+     * find address of customer
+     * @param address of customer
+     * @return list of address
+     */
+    public List<Customer> findByAddressContainingCase(String address);
+
+    /**
+     * Find a exact name
+     */
+    Optional<Customer> findByName(String name);
+
+    /**
+     * Search with geographic area
+     */
+    @Query("SELECT c FROM Customer c WHERE " +
+            "SQRT(POWER(c.latitude - :lat, 2) + POWER(c.longitude - :lon, 2)) * 111.32 <= :radiusKm")
+    List<Customer> findCustomersWithRadius(
+            @Param("lat") Double latitude,
+            @Param("long") Double longitude,
+            @Param("radiusKm") Double radiusKm
+    );
+
+    /**
+     * Find customer with pending status of theirs delivery
+     */
+    @Query("SELECT DISTINCT c FROM Customer c"
+         + "JOIN c.delivery d" + "WHERE d.status = 'PENDING'")
+    List<Customer> findCustomersWithPendingDeliveries();
+
+    /**
+     * Customer static
+     */
+    @Query("SELECT c.id, c.name, COUNT(d) as totalDeliveries, " +
+            "SUM(CASE WHEN d.status = 'DELIVERED' THEN 1 ELSE 0 END) as completedDeliveries " +
+            "FROM Customer c LEFT JOIN c.deliveries d " +
+            "GROUP BY c.id, c.name")
+    List<Objects[]> getCustomerStatistics();
+}
