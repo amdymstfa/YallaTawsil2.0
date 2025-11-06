dbc118f4a405e387e83ffa47d18d4dc337440d8c
amdymstfa <amdymoustapha011@gmail.com>
2025-11-06 12:44:49 +0100

    Improve CustomerMapper: add Javadoc and clarify entity/DTO conversion methods

--- src/main/java/com/yallatawsil/backend/mapper/CustomerMapper.java
+++ src/main/java/com/yallatawsil/backend/mapper/CustomerMapper.java
@@ -1,15 +1,20 @@
-package com.yallatawsil.backend.mapper ;
+package com.yallatawsil.backend.mapper;
 
 import com.yallatawsil.backend.dto.request.CustomerRequestDTO;
 import com.yallatawsil.backend.dto.response.CustomerResponseDTO;
-import org.mapstruct.Mapper;
 import com.yallatawsil.backend.entity.Customer;
+import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface CustomerMapper {
 
-    // Convert DTO to Entity
+    /**
+     * Convert Customer entity to CustomerResponseDTO
+     */
     CustomerResponseDTO toResponseDTO(Customer customer);
-    // Convert Entity to DTO
+
+    /**
+     * Convert CustomerRequestDTO to Customer entity
+     */
     Customer toEntity(CustomerRequestDTO dto);
 }
