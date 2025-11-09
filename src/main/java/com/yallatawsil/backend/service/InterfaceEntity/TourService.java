efec761ff0fe3b892fd9c2a3d3385aa5ad5592b4
amdymstfa <amdymoustapha011@gmail.com>
2025-11-09 18:04:33 +0100

    feat(service): declare updateTourStatus method with @Transactional annotation

--- src/main/java/com/yallatawsil/backend/service/InterfaceEntity/TourService.java
+++ src/main/java/com/yallatawsil/backend/service/InterfaceEntity/TourService.java
@@ -3,6 +3,8 @@ package com.yallatawsil.backend.service.InterfaceEntity;
 import com.yallatawsil.backend.dto.request.TourRequestDTO;
 import com.yallatawsil.backend.dto.response.OptimizationComparisonDTO;
 import com.yallatawsil.backend.dto.response.TourResponseDTO;
+import com.yallatawsil.backend.entity.enums.TourStatus;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,4 +21,7 @@ public interface TourService {
     OptimizationComparisonDTO compareAlgorithms(TourRequestDTO dto);
 
     Double getTotalDistance(Long id);
+
+    @Transactional
+    TourResponseDTO updateTourStatus(Long tourId, TourStatus newStatus);
 }
