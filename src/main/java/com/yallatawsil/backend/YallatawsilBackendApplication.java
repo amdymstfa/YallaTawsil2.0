260a6a0f75ef022d54872c56466f939e5b4aa401
amdymstfa <amdymoustapha011@gmail.com>
2025-11-12 15:30:45 +0100

    Update SpringBootApplication scanBasePackages and remove println

--- src/main/java/com/yallatawsil/backend/YallatawsilBackendApplication.java
+++ src/main/java/com/yallatawsil/backend/YallatawsilBackendApplication.java
@@ -2,15 +2,11 @@ package com.yallatawsil.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication(scanBasePackages = {})
+@SpringBootApplication(scanBasePackages = "com.yallatawsil.backend")
 
 public class YallatawsilBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(YallatawsilBackendApplication.class, args);
-        System.out.println("\n=========================================================\n" +
-                " Yalla Tawsil Backend - Delivery Route Optimization System\n" +
-                "=========================================================\n");
     }
 }
